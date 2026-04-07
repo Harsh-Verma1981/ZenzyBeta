7495826100b229eb44142067ae2e9ad41622ac79
Harsh-Verma1981 <harshv2208@gmail.com>
2026-04-07 21:23:01 +0530

    fixing user order

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,9 +1,19 @@
+// export const BASE_URL = "https://zenzloom-fg7a.onrender.com";
+
+// // Use the BASE_URL to create full paths
+// export const USERS_URL = `${BASE_URL}/api/users`;
+// export const CATEGORY_URL = `${BASE_URL}/api/category`;
+// export const PRODUCT_URL = `${BASE_URL}/api/products`;
+// export const UPLOAD_URL = `${BASE_URL}/api/upload`;
+// export const ORDERS_URL = `${BASE_URL}/api/orders`;
+// export const RAZORPAY_URL = `${BASE_URL}/api/config/razorpay`;
+
 export const BASE_URL = "https://zenzloom-fg7a.onrender.com";
 
-// Use the BASE_URL to create full paths
-export const USERS_URL = `${BASE_URL}/api/users`;
-export const CATEGORY_URL = `${BASE_URL}/api/category`;
-export const PRODUCT_URL = `${BASE_URL}/api/products`;
-export const UPLOAD_URL = `${BASE_URL}/api/upload`;
-export const ORDERS_URL = `${BASE_URL}/api/orders`;
-export const RAZORPAY_URL = `${BASE_URL}/api/config/razorpay`;
+// DO NOT use ${BASE_URL} here, RTK Query adds it automatically from apiSlice
+export const USERS_URL = "/api/users";
+export const CATEGORY_URL = "/api/category";
+export const PRODUCT_URL = "/api/products";
+export const UPLOAD_URL = "/api/upload";
+export const ORDERS_URL = "/api/orders";
+export const RAZORPAY_URL = "/api/config/razorpay";
