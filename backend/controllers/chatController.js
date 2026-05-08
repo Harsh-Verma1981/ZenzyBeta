6de7e34fa1d2cd250792b5170afb9840af5df7fa
Harsh-Verma1981 <harshv2208@gmail.com>
2026-05-08 10:49:59 +0530

    migrate genai package

--- backend/controllers/chatController.js
+++ backend/controllers/chatController.js
@@ -1,4 +1,4 @@
-import { GoogleGenerativeAI } from "@google/generative-ai";
+import { GoogleGenerativeAI } from "@google/genai";
 
 export const chatWithAI = async (req, res) => {
   const { message } = req.body;
